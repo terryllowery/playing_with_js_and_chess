9c0df5cd609b606ce03688d2593fa7b5923706c3
Unknown <terry@lowerytech.net>
2017-12-28 14:52:41 -0600

    initial commit

---
+++ board.js
@@ -0,0 +1,38 @@
+var boardContainer = document.createElement('div');
+boardContainer.setAttribute("class", "board-container");
+var table = document.createElement('table');
+table.setAttribute("id", "board");
+
+
+
+$(function () {
+    $('.board-container').append(table);
+
+    var row = "";
+
+    console.info(row);
+
+    for (var i = 0; i < 8; i++) {
+        var tableData = "";
+        // var tableRow = document.createElement('tr');
+        row += "<tr class='" + i + "'>" +
+            "\n";
+
+        for(var n = 0; n < 8; n++) {
+            tableData += "<td id='" + n + "'>"
+                + "Location: " + i + ":" +
+                n +
+                "</td>" + "\n";
+            // tableRow.appendChild(document.createElement('td') );
+            // table.appendChild(tableRow);
+        }
+        row += tableData + "</tr>" + "\n";
+        console.info(i);
+        console.info(row);
+
+
+    }
+    $("#board").append(row);
+
+});
+
