63be2876126ac0926a7137e7ebb33538b9d4e580
Unknown <terry@lowerytech.net>
2017-12-29 12:50:38 -0600

    Updated js code
    Added function to change input to string. 
    Added code to call into .text change on page load

--- board.js
+++ board.js
@@ -1,10 +1,21 @@
+// Create div container and assign it a named class.
+// This is for CSS to show it correctly
 var boardContainer = document.createElement('div');
 boardContainer.setAttribute("class", "board-container");
+
+// Create a able with an id so we can target it in updates
 var table = document.createElement('table');
 table.setAttribute("id", "board");
 
+// Just takes the input and changes in to string to send to a function to change the
+// square.
+function send_location_change(row, col){
+    var row = row.toString();
+    var col = col.toString();
+    return [row, col];
+}
 
-
+// Creates the board and populates the ID's inside the table
 $(function () {
     $('.board-container').append(table);
 
@@ -34,5 +45,10 @@ $(function () {
     }
     $("#board").append(row);
 
-});
+    // Testing purposes - just change the square in 1, 3 to KING
+    var target = send_location_change(1, 3);
+    var targetId = "table#board tr#row_" + target[0] + " td#col_" + target[1];
+    $(targetId).text("KING");
 
+
+});
